13a07fc14eca63123ddfbc2b1624f078b4df5f7b
nishmitha-r <nishabhat2001@gmail.com>
2023-07-19 18:52:28 +0530

    Added greet method for /greet url

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello !</h1>"
 
+@app.route('/greet')
+def greet():
+    return "Hello Nisha"
+
 if __name__ == "__main__":
     app.run(debug=True)
